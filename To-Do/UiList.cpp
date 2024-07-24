eb5cf0278cacbb6c1d3bb40acd8c3d006038a9eb
jlorenz1 <joellorenz888@gmail.com>
2024-07-24 14:59:08 -0400

    Simple functions for ui

--- To-Do/UiList.cpp
+++ To-Do/UiList.cpp
@@ -1,1 +1,10 @@
 #include "UIList.h"
+
+std::string UIList::getName()
+{
+    return std::string();
+}
+
+void UIList::displayList(std::vector<std::string> list)
+{
+}
--- To-Do/UiList.h
+++ To-Do/UiList.h
@@ -1,7 +1,14 @@
 #pragma once
+#include <vector>
+#include <string>
+
 class UIList
 {
 public: 
+	std::vector<std::string> testVector; 
+
+	std::string getName();
+	void displayList(std::vector<std::string> list);
 
 private:
 
